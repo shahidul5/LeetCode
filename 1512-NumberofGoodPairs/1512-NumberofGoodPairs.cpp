70b359c78fb17878d508734586dd4515675c8ba0
Md. Shahid-ul Islam <43965492+dev-shahidul@users.noreply.github.com>
2024-02-12 00:03:55 +0600

    Create file

---
+++ 1512-NumberofGoodPairs/1512-NumberofGoodPairs.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+    public:
+        int numIdenticalPairs(vector<int> &nums)
+        {
+            int a = 0;
+            for (int i = 0; i < nums.size() - 1; i++)
+            {
+                for (int j = 1; j < nums.size(); j++)
+                {
+                    if (nums[i] == nums[j] && i < j) a++;
+                }
+[1,2,3,1,1,3]
