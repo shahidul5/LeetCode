ed89b9cb9bc2c105e1b3cadb1088d443a877245c
Md. Shahid-ul Islam <43965492+dev-shahidul@users.noreply.github.com>
2024-03-27 23:50:25 +0600

    Time: 44 ms (50.01%), Space: 35.7 MB (96.53%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        unordered_set<int> hashSet;
         vector<int> result;
-        for(int i = 0; i < nums.size(); i++){
-            if(hashSet.find(nums[i]) != hashSet.end()) result.push_back(nums[i]);
-            else hashSet.insert(nums[i]);
+        for (const int num : nums) {
+            nums[abs(num) - 1] *= -1;
+            if (nums[abs(num) - 1] > 0) result.push_back(abs(num));
         }
+        
         return result;
     }
 };
