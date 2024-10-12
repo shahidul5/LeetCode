148bf23ca77df6598da4544f861e04009f1fb76a
Md. Shahid-ul Islam <43965492+shahidul5@users.noreply.github.com>
2024-10-12 16:53:59 +0000

    Add 1768 problem

---
+++ 1768-merge-strings-alternatively/1768-merge-strings-alternatively.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int maxLength = max(word1.size(), word2.size());
+        
+        for(int i = 0; i < maxLength; i++) {
+            if (i < word1.size()) {
+                result += word1[i];
+            }
+            if (i < word2.size()) {
+                result += word2[i];
+            }
+        }
+        
+        return result;
+    }
+};
