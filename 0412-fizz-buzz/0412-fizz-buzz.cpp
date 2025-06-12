b02862c43d2ed6dee4e0974e725e18a36b0ecccb
Md. Shahid-ul Islam <43965492+shahidul5@users.noreply.github.com>
2025-06-12 21:07:34 +0600

    Time: 0 ms (100%), Space: 11.1 MB (93.36%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> answer;
+        vector<string> answer(n);
         for(int i = 1; i <= n; i++) {
             if (i == 1 || (i % 2 == 0 && i % 3 != 0 && i % 5 != 0)) {
-                answer.push_back(to_string(i));
+                answer[i-1] = to_string(i);
                 continue;
             }
             if (i % 3 == 0 && i % 5 == 0) {
-                answer.push_back("FizzBuzz");
+                answer[i-1] = "FizzBuzz";
                 continue;
             }
             if (i % 3 == 0) {
-                answer.push_back("Fizz");
+                answer[i-1] = "Fizz";
                 continue;
             }
             if (i % 5 == 0) {
-                answer.push_back("Buzz");
+                answer[i-1] = "Buzz";
                 continue;
             }
-            answer.push_back(to_string(i));
+            answer[i-1] = to_string(i);
         }
         return answer;
     }
